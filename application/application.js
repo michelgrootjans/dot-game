a9ec00f47e8895bf34b1577ad8637f10276dca88
Michel Grootjans <michel.grootjans@gmail.com>
2021-08-29 17:13:21 +0200

    implemented iteration timer

--- application/application.js
+++ application/application.js
@@ -1,24 +1,29 @@
 const {IterationStarted, IterationFinished} = require("./api/events/iteration");
+const {EndIteration} = require("./api/commands/iteration");
 
-const Application = ({publish}) => {
+const Application = ({publish, subscribe}) => {
   const iterations = []
 
-  return {
-    execute: command => {
-      switch (command.type) {
-        case 'StartIteration':
-          iterations.push(command.iterationId)
-          publish(IterationStarted(command.iterationId));
-          break;
-        case 'EndIteration':
-          if(iterations.includes(command.iterationId))
-            publish(IterationFinished(command.iterationId));
-          break;
-        default:
-          throw `Unknown command: ${command}`;
-      }
+  const execute = command => {
+    switch (command.type) {
+      case 'StartIteration':
+        iterations.push(command.iterationId);
+        publish(IterationStarted(command.iterationId));
+        break;
+      case 'EndIteration':
+        if (iterations.includes(command.iterationId))
+          publish(IterationFinished(command.iterationId));
+        break;
+      default:
+        throw `Unknown command: ${command}`;
     }
+  };
+
+  subscribe('IterationStarted', event => setTimeout(() => execute(EndIteration(event.iterationId)), 1000))
+
+  return {
+    execute
   }
-}
+};
 
 module.exports = Application
--- spec/iteration.spec.js
+++ spec/iteration.spec.js
@@ -2,13 +2,34 @@ const Application = require("../application/application");
 const {StartIteration, EndIteration} = require("../application/api/commands/iteration");
 const {IterationStarted, IterationFinished} = require("../application/api/events/iteration");
 
+const EventBus = () => {
+  const subscribers = []
+  const listeners = []
+
+  const subscribe = (eventType, handler) => subscribers.push({eventType, handler})
+  const publish = event => {
+    subscribers.filter(subscriber => subscriber.eventType === event.type)
+      .forEach(subscriber => subscriber.handler(event));
+    listeners.forEach(listener => listener(event));
+  }
+  const tap = listener => listeners.push(listener)
+
+  return {
+    publish,
+    subscribe,
+    tap
+  }
+};
+
 describe('Iteration', () => {
   let application = undefined;
   let events = undefined;
 
   beforeEach(function () {
     events = [];
-    application = Application({publish: event => events.push(event)});
+    const eventBus = EventBus();
+    eventBus.tap(event => events.push(event))
+    application = Application(eventBus);
   });
 
   it('can start', function () {
@@ -31,4 +52,19 @@ describe('Iteration', () => {
     application.execute(EndIteration('i1'))
     expect(events).toMatchObject([]);
   });
+
+  describe('Process manager', () => {
+    beforeEach(jest.useFakeTimers);
+    afterEach(jest.useRealTimers);
+
+    it('ends the iteration after a timeout', function () {
+      application.execute(StartIteration('i1'))
+      jest.runAllTimers();
+      expect(events).toMatchObject([
+        IterationStarted('i1'),
+        IterationFinished('i1')
+      ]);
+    });
+  });
 });
+
