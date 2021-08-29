f69dd3e2a38f899468d7602a4fc6e3ec2698c114
Michel Grootjans <michel.grootjans@gmail.com>
2021-08-29 14:56:02 +0200

    first basic test

--- spec/getting-started.spec.js
+++ spec/getting-started.spec.js
@@ -1,5 +1,24 @@
-describe('no idea', () => {
+const Application = (publish) => {
+  return {
+    execute: () => {publish(IterationStarted())}
+  }
+}
+const StartIteration = () => {}
+const IterationStarted = () => {}
+
+describe('a simple iteration', () => {
+  let application = undefined;
+  let events = undefined;
+
+  beforeEach(function () {
+    events = [];
+    application = Application(event => events.push(event));
+  });
+
   it('should work', function () {
-    expect(true).toBeTruthy();
+    application.execute(StartIteration())
+    expect(events).toMatchObject([
+      IterationStarted()
+    ]);
   });
 });
