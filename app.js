7944c85400496984dcc41fecba9ae3be67245904
Michel Grootjans <michel.grootjans@gmail.com>
2021-08-29 20:58:51 +0200

    trying to wire it all together

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// var socketApi = require('../socketapi')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- routes/games.js
+++ routes/games.js
@@ -1,12 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {StartIteration} = require("../application/api/commands/iteration");
+const { v4: uuidv4 } = require('uuid');
 
 const init = application => {
   console.log(`initialized with ${application.name}`)
 
   router.get('/', function(req, res, next) {
-    console.log(`called with ${application.name}`)
     res.send({ title: `Dot Game v2 - ${application.name}` });
+  });
+
+  router.post('/:gameId/iteration', function (req, res, next) {
+    const command = StartIteration(uuidv4());
+    application.execute(command)
+    res.send({params: req.params, body: req.body, iterationId: command.iterationId});
   });
 
   return router;
