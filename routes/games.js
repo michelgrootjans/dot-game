fb14e5cb781df80af72570c9e1add4bbc6a90886
Michel Grootjans <michel.grootjans@gmail.com>
2021-08-30 00:03:20 +0200

    green

--- routes/games.js
+++ routes/games.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const {StartIteration} = require("../application/api/commands/iteration");
-const { v4: uuidv4 } = require('uuid');
 const {CreateGame} = require("../application/api/commands/game");
 
 const init = application => {
 
-  router.get('/', function(req, res, next) {
-    res.send({ title: 'Dot Game v2' });
+  router.post('/', function (req, res, next) {
+    const command = CreateGame(req.body.gameId);
+    application.execute(command);
+    res.send({params: req.params, body: req.body, command: JSON.stringify(command)});
   });
 
   router.post('/:gameId/iteration', function (req, res, next) {
-    const gameId = req.params.gameId;
-    application.execute(CreateGame(gameId));
-    const command = StartIteration(gameId, uuidv4());
+    const command = StartIteration(req.params.gameId, req.body.iterationId);
     application.execute(command)
-    res.send({params: req.params, body: req.body, iterationId: command.iterationId});
+    res.send({params: req.params, body: req.body, command: JSON.stringify(command)});
   });
 
   return router;
