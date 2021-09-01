c6445ce3cfd21ee84ee51eaff10d07ec185fe53f
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-01 08:30:48 +0200

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dot Game' });
+  res.render('index', { title: 'The Dot Game' });
 });
 
 module.exports = router;
