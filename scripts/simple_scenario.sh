2d2c21eeb769ed5184ad96c7790fe0b67daead63
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-02 22:10:52 +0200

    tuned the script

--- scripts/simple_scenario.sh
+++ scripts/simple_scenario.sh
@@ -1,38 +1,38 @@
-#http -f POST :3000/api/games/ gameId=default
-http -f POST :3000/api/games/default/iterations duration=5000
+http -f POST :3000/api/games/default/iterations duration=10000
 
 http -f POST :3000/api/games/default/tasks taskId='t1'
+http -f POST :3000/api/games/default/tasks/t1/move
 http -f POST :3000/api/games/default/tasks taskId='t2'
 http -f POST :3000/api/games/default/tasks taskId='t3'
 http -f POST :3000/api/games/default/tasks taskId='t4'
+http -f POST :3000/api/games/default/tasks taskId='t5'
+http -f POST :3000/api/games/default/tasks taskId='t6'
+http -f POST :3000/api/games/default/tasks taskId='t7'
+http -f POST :3000/api/games/default/tasks taskId='t8'
+http -f POST :3000/api/games/default/tasks taskId='t9'
+http -f POST :3000/api/games/default/tasks taskId='t10'
+
+http -f POST :3000/api/games/default/tasks/t1/move
+http -f POST :3000/api/games/default/tasks/t1/move
+http -f POST :3000/api/games/default/tasks/t2/move
 
 sleep 1
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t1/move
-sleep 1
-http -f POST :3000/api/games/default/tasks/t2/move
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t3/move
-sleep 1
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t3/move
-sleep 1
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t2/move
-http -f POST :3000/api/games/default/tasks/t4/move
-sleep 1
-http -f POST :3000/api/games/default/tasks/t1/move
 http -f POST :3000/api/games/default/tasks/t2/move
 http -f POST :3000/api/games/default/tasks/t3/move
+
 sleep 1
-http -f POST :3000/api/games/default/tasks/t1/move
-http -f POST :3000/api/games/default/tasks/t2/move
 http -f POST :3000/api/games/default/tasks/t3/move
 http -f POST :3000/api/games/default/tasks/t4/move
+http -f POST :3000/api/games/default/tasks/t1/move
+http -f POST :3000/api/games/default/tasks/t1/move
+http -f POST :3000/api/games/default/tasks/t2/move
+
+sleep 1
+http -f POST :3000/api/games/default/tasks/t3/move
+http -f POST :3000/api/games/default/tasks/t4/move
+http -f POST :3000/api/games/default/tasks/t5/move
+http -f POST :3000/api/games/default/tasks/t1/move
+http -f POST :3000/api/games/default/tasks/t1/move
+http -f POST :3000/api/games/default/tasks/t2/move
+
+sleep 1
