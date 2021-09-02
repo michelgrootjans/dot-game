90158447902e90c2af08490667f22fbd08464c2f
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-02 17:24:11 +0200

    added a create task button

--- web/src/iteration.js
+++ web/src/iteration.js
@@ -1,14 +1,21 @@
 const $startIterationButton = document.getElementById('start-iteration');
+const $createTaskButton = document.getElementById('create-task');
 
 const StartIteration = event => {
   return {
-    handle: () => $startIterationButton.disabled = true
+    handle: () => {
+      $startIterationButton.disabled = true;
+      $createTaskButton.disabled = false;
+    }
   }
 };
 
 const FinishIteration = event => {
   return {
-    handle: () => $startIterationButton.disabled = false
+    handle: () => {
+      $startIterationButton.disabled = false;
+      $createTaskButton.disabled = true;
+    }
   }
 };
 
