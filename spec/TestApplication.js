ba4e10397de79b0647f50d45510dc59b857170cd
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-04 22:58:08 +0200

    simplified tests with TestApplication

--- spec/TestApplication.js
+++ spec/TestApplication.js
@@ -2,16 +2,21 @@ const GamesRepository = require("../application/GameRepository");
 const StatsRepository = require("../application/StatsRepository");
 const EventBus = require("../application/EventBus");
 const Application = require("../application/Application");
+const FakeTimer = require("./FakeTimer");
 
 const TestApplication = () => {
   const games = GamesRepository();
   const stats = StatsRepository();
   const {publish, subscribe} = EventBus();
   const delay = () => {};
-  const application = Application({games, stats, publish, subscribe, delay});
+  const timer = FakeTimer();
+  const application = Application({games, stats, publish, subscribe, delay, currentTime: timer.currentTime});
 
   return {
-    ...application
+    ...application,
+    publish,
+    currentTime: timer.currentTime,
+    advanceTime: timer.advance
   };
 };
 
--- spec/stats.spec.js
+++ spec/stats.spec.js
@@ -6,32 +6,23 @@ const {TaskCreated, TaskFinished} = require("../application/api/events/task");
 const Application = require("../application/Application");
 const FakeTimer = require("./FakeTimer");
 const StatsRepository = require("../application/StatsRepository");
+const TestApplication = require("./TestApplication");
 
 describe('stats end-to-end', () => {
 
   it("keeps history", () => {
-    const games = GameRepository();
-    const stats = StatsRepository();
-    const {publish, subscribe} = EventBus();
-    const timer = FakeTimer()
-    const {execute, findStats} = Application({
-      games, stats, publish, subscribe,
-      delay: () => {
-      },
-      currentTime: timer.currentTime
-    });
+    application = TestApplication();
 
+    application.execute(CreateGame({gameId: 'g1'}));
+    application.execute(StartIteration({gameId: 'g1', duration: 0}));
 
-    execute(CreateGame({gameId: 'g1'}));
-    execute(StartIteration({gameId: 'g1', duration: 0}));
+    application.advanceTime(1);
+    application.publish(TaskCreated({gameId: 'g1', taskId: 't1', columnId: 'c1'}))
 
-    timer.advance(1);
-    publish(TaskCreated({gameId: 'g1', taskId: 't1', columnId: 'c1'}))
+    application.advanceTime(1);
+    application.publish(TaskFinished({gameId: 'g1', taskId: 't1'}))
 
-    timer.advance(1);
-    publish(TaskFinished({gameId: 'g1', taskId: 't1'}))
-
-    expect(findStats('g1')).toMatchObject({
+    expect(application.findStats('g1')).toMatchObject({
       history: [{time: 0, wip: 0}, {time: 1, wip: 1}, {time: 2, wip: 0}],
     })
   });
