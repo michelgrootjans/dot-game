a95c465d89990dfbd4b565efe56486158093b6db
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-07 19:13:05 +0200

    taskIds are now UUID

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,7 @@
 import {io} from "socket.io-client";
 import {FinishIteration, StartIteration} from "./iteration";
 import {TaskCreated, TaskMoved} from "./task";
+import {v4 as uuidv4} from 'uuid';
 
 const currentGameId = document.getElementById('gameId').value
 
@@ -26,9 +27,6 @@ socket.on('message', function (event) {
   handlerForEvent(event).handle(event)
 });
 
-let taskCounter = 1;
-const nextTaskId = () => `${taskCounter++}`;
-
 const initializeGame = gameId => {
   $startIterationButton.addEventListener('click', () => {
     fetch(`/api/games/${gameId}/iterations`, {
@@ -41,7 +39,7 @@ const initializeGame = gameId => {
     fetch(`/api/games/${gameId}/tasks`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({taskId: nextTaskId()})
+      body: JSON.stringify({taskId: uuidv4()})
     })
   });
 };
