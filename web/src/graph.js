238ad9327bc055a911f36d1c6d76b91eb1e860ac
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-07 19:08:52 +0200

    its alive

--- web/src/graph.js
+++ web/src/graph.js
@@ -1,43 +1,95 @@
-const {Chart, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale} = require('chart.js')
+const {Chart, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale, TimeScale, TimeScaleOptions, TimeSeriesScale} = require('chart.js')
 
-Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
+Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale, TimeScale, TimeScaleOptions, TimeSeriesScale);
 
 const ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    datasets: [{
-      label: '# of Votes',
-      data: [{x: 'Red', y: 12}, {x: 'Blue', y: 19}, {x: 'Yellow', y: 3}, {x: 'Green', y: 5}, {x: 'Purple', y: 2}, {x: 'Orange', y: 3}],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
 
-chart.update();
+function newDate(number) {
+  return Date.now() + number * 1000 * 60 * 24;
+}
+
+let Utils = {
+  CHART_COLORS: {red: 1},
+
+  newDate(number) {
+    return newDate(number);
+  },
+  transparentize() {
+    return '#673546';
+  },
+  newDateString(number) {
+    return newDate(number).toString();
+  },
+  rand(number, number2) {
+    return number + Math.random() * number2;
+  }
+};
+const data = {
+  labels: [ // Date Objects
+    Utils.newDate(0),
+    Utils.newDate(1),
+    Utils.newDate(2),
+    Utils.newDate(3),
+    Utils.newDate(4),
+    Utils.newDate(5),
+    Utils.newDate(6)
+  ],
+  datasets: [{
+    label: 'Dataset with point data',
+    backgroundColor: Utils.transparentize(),
+    fill: false,
+    data: [{
+      x: Utils.newDateString(0),
+      y: Utils.rand(0, 100)
+    }, {
+      x: Utils.newDateString(5),
+      y: Utils.rand(0, 100)
+    }, {
+      x: Utils.newDateString(7),
+      y: Utils.rand(0, 100)
+    }, {
+      x: Utils.newDateString(15),
+      y: Utils.rand(0, 100)
+    }],
+  }]
+};
+
+const config = {
+  type: 'line',
+  data: data,
+  options: {
+    plugins: {
+      title: {
+        text: 'Chart.js Time Scale',
+        display: true
+      }
+    },
+    scales: {
+      x: {
+        type: 'time',
+        time: {
+          // Luxon format string
+          tooltipFormat: 'DD T'
+        },
+        title: {
+          display: true,
+          text: 'Date'
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'value'
+        }
+      }
+    },
+  },
+};
+
+
+var myChart = new Chart(ctx, config);
+
+myChart.update();
 
 const Graph = () => {
   const update = () => {
