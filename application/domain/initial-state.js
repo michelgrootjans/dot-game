d8a3249293636443ea2db751bc9080e2b7407fde
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-09 13:31:39 +0200

    added a dash of styling to the board

--- application/domain/initial-state.js
+++ application/domain/initial-state.js
@@ -1,6 +1,6 @@
 const initialState = () => ({
     columns: [
-      {columnId: "c1", columnType: "queue-column", nextColumnId: "c2", title: "To do", taskName: "todo"},
+      {columnId: "c1", columnType: "start-column", nextColumnId: "c2", title: "To do", taskName: "todo"},
       {columnId: "c2", columnType: "work-column", nextColumnId: "c3", title: "Analysis", taskName: "analysis"},
       {columnId: "c3", columnType: "queue-column", nextColumnId: "c4", title: "Ready for design", taskName: "analysis"},
       {columnId: "c4", columnType: "work-column", nextColumnId: "c5", title: "Design", taskName: "design"},
@@ -8,7 +8,7 @@ const initialState = () => ({
       {columnId: "c6", columnType: "work-column", nextColumnId: "c7", title: "Development", taskName: "development"},
       {columnId: "c7", columnType: "queue-column", nextColumnId: "c8", title: "Ready for QA", taskName: "development"},
       {columnId: "c8", columnType: "work-column", nextColumnId: "c9", title: "QA", taskName: "qa"},
-      {columnId: "c9", columnType: "queue-column", title: "Done", taskName: "done"}
+      {columnId: "c9", columnType: "end-column", title: "Done", taskName: "done"}
     ]
   }
 )
