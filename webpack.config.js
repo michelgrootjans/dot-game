38f12e8a63596d60b8982086bd9910fe29daf373
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-12 21:44:52 +0200

    fixed work page

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    'dot-game-board': './web/src/board.js',
-    'dot-game-work': './web/src/work.js',
+    'dot-game': './web/src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'web', 'dist'),
