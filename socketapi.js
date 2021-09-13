ac6682ed6cac8cf0790c010aadd5f791cf983e34
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-13 09:06:11 +0200

    naive reload all events on page refresh

--- socketapi.js
+++ socketapi.js
@@ -2,15 +2,21 @@ const { Server } = require("socket.io");
 
 let init = server => {
   const io = new Server(server);
+  const previousEvents = []
 
   const publish = event => {
     console.log({message: event})
+    previousEvents.push(event);
     io.emit('message', event)
   };
 
   io.on('connection', (socket) => {
 
     publish({type: 'UserJoined'});
+    previousEvents.forEach(e => {
+      console.log({e})
+      socket.emit('message', e);
+    });
 
     socket.on('disconnect', () => {
       publish({type: 'UserLeft'});
