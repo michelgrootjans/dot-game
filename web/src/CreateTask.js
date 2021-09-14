8d3837c86daf4fcc3a8b44be9dbfa75d096b6cf3
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-14 14:37:54 +0200

    limit the speed at which items can be created

--- web/src/CreateTask.js
+++ web/src/CreateTask.js
@@ -4,10 +4,15 @@ const initialize = gameId => {
   const $createTaskButton = document.getElementById('create-task');
   if(!$createTaskButton) return;
 
-  document.addEventListener('IterationStarted', () => $createTaskButton.disabled = false)
-  document.addEventListener('IterationFinished', () => $createTaskButton.disabled = true)
+  const enableCreateion = () => $createTaskButton.disabled = false;
+  const disableCreateion = () => $createTaskButton.disabled = true;
+
+  document.addEventListener('IterationStarted', enableCreateion)
+  document.addEventListener('IterationFinished', disableCreateion)
 
   $createTaskButton.addEventListener('click', () => {
+    disableCreateion();
+    setTimeout(enableCreateion, 1000);
     fetch(`/api/games/${gameId}/tasks`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
