2bae411ae77eed74dcbcc8fb12979137fe335aa5
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-17 09:32:44 +0200

    fixed typo

--- web/src/CreateTask.js
+++ web/src/CreateTask.js
@@ -4,15 +4,15 @@ const initialize = gameId => {
   const $createTaskButton = document.getElementById('create-task');
   if(!$createTaskButton) return;
 
-  const enableCreateion = () => $createTaskButton.disabled = false;
-  const disableCreateion = () => $createTaskButton.disabled = true;
+  const enableCreation = () => $createTaskButton.disabled = false;
+  const disableCreation = () => $createTaskButton.disabled = true;
 
-  document.addEventListener('IterationStarted', enableCreateion)
-  document.addEventListener('IterationFinished', disableCreateion)
+  document.addEventListener('IterationStarted', enableCreation)
+  document.addEventListener('IterationFinished', disableCreation)
 
   $createTaskButton.addEventListener('click', () => {
-    disableCreateion();
-    setTimeout(enableCreateion, 1000);
+    disableCreation();
+    setTimeout(enableCreation, 1000);
     fetch(`/api/games/${gameId}/tasks`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
