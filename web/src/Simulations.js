d0972afd55806a838aa4592a401bb9c18023522e
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-17 16:18:58 +0200

    introduced a hidden simulation

--- web/src/Simulations.js
+++ web/src/Simulations.js
@@ -1,10 +1,38 @@
-const Card = require("./Card");
+const API = require("./API");
 
 const initialize = (gameId) => {
+  const api = API(gameId)
+  let iteration = undefined;
+
+  const createTask = () => {
+    console.log('PING')
+  };
+  const onTaskCreated = ({detail}) => {
+  };
+  const onTaskMoved = ({detail}) => {
+  };
+  const onTaskFinished = ({detail}) => {
+  };
+
   window.runSimulation = (param) => {
-    document.addEventListener('TaskCreated', ({detail}) => {
+    let timerHandle = undefined;
+    document.addEventListener('IterationStarted', ({detail}) => {
+      console.log('START')
+      iteration = detail;
+      timerHandle = setInterval(createTask, 1000)
+      document.addEventListener('TaskCreated', onTaskCreated);
+      document.addEventListener('TaskMoved', onTaskMoved);
+      document.addEventListener('TaskFinished', onTaskFinished);
     });
-  }
+    document.addEventListener('IterationFinished', () => {
+      console.log('FINISH')
+      clearInterval(timerHandle);
+      document.removeEventListener('TaskCreated', onTaskCreated);
+      document.removeEventListener('TaskMoved', onTaskMoved);
+      document.removeEventListener('TaskFinished', onTaskFinished);
+    });
+    api.iteration.start({duration: 60000})
+  };
 
 }
 
