6c66232b69d91f0f41880f975b96acda9d952776
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-21 15:33:34 +0200

    experiment with webpack production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: 'production',
+  // devtool: 'inline-source-map',
   entry: {
     'dot-game': './web/src/index.js'
   },
