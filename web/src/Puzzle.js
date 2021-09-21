53174cb478f237131115bb819632ba6bb4a40734
Michel Grootjans <michel.grootjans@gmail.com>
2021-09-21 14:17:36 +0200

    tuned the puzzle exercises

--- web/src/Puzzle.js
+++ web/src/Puzzle.js
@@ -1,19 +1,29 @@
 const Puzzle = () => {
   const any = array => array[Math.floor(Math.random() * array.length)];
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const operators = ['+', '-', 'x', '/'];
+
+  function operatorFor(difficulty) {
+    if(difficulty == 2) return any(['x', '/']);
+    if(difficulty == 3) return any(['+', '-']);
+    return any(['+', '-', 'x', '/']);
+  }
 
   const generate = (difficulty) => {
-    const operator = any(operators)
+    if(difficulty == 1) {
+      const number = any(numbers);
+      return {question: `${number} = `, answer: `${number}`};
+    }
+
+    const operator = operatorFor(difficulty)
 
     if (operator === '+') {
-      const first = 5 * (difficulty - 1) * any(numbers) + any(numbers)
-      const second = 5 * (difficulty - 1) * any(numbers) + any(numbers)
+      const first = any(numbers) * 10 + any(numbers)
+      const second = any(numbers) * 10 + any(numbers)
       return {question: `${first} + ${second} = `, answer: `${first + second}`}
     }
     if (operator === '-') {
-      const first = 10 * (difficulty - 1) * any(numbers) + any(numbers)
-      const second = 5 * (difficulty - 1) * any(numbers) + any(numbers)
+      const first = any(numbers) * 10 + any(numbers)
+      const second = any(numbers) * 10 + any(numbers)
       return {question: `${Math.max(first, second)} - ${Math.min(first, second)} = `, answer: `${Math.abs(first - second)}`}
     }
     if (operator === 'x') {
@@ -22,7 +32,7 @@ const Puzzle = () => {
       return {question: `${first} x ${second} = `, answer: `${first * second}`}
     }
     if (operator === '/') {
-      const first = (any(numbers) + 1) * difficulty
+      const first = any(numbers) + 1
       const second = any(numbers)
       return {question: `${first*second} / ${first} = `, answer: `${second}`}
     }
