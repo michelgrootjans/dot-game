5806d32f6d1fab8f8c13e9cb330392c5b48310e7
Michel Grootjans <michel.grootjans@gmail.com>
2022-04-27 13:35:30 +0200

    initialize the game only if the gameId is known

--- web/src/index.js
+++ web/src/index.js
@@ -9,27 +9,32 @@ import IterationStats from "./IterationStats";
 import Testing from "./Testing";
 import Simulations from "./Simulations";
 
-const initializeGame = (gameId) => {
-  StartIteration.initialize(gameId);
-  CreateTask.initialize(gameId);
-  Columns.initialize();
-  Workspace.initialize();
-  Testing.initialize();
-  Charts.initialize(gameId);
-  ProgressBar.initialize();
-  IterationStats.initialize(gameId);
 
-  Simulations.initialize(gameId);
-};
+const $gameId = document.querySelector('[data-game-id]');
+if ($gameId) {
+  const initializeGame = (gameId) => {
+    StartIteration.initialize(gameId);
+    CreateTask.initialize(gameId);
+    Columns.initialize();
+    Workspace.initialize();
+    Testing.initialize();
+    Charts.initialize(gameId);
+    ProgressBar.initialize();
+    IterationStats.initialize(gameId);
 
-const gameId = document.querySelector('[data-game-id]').dataset.gameId;
-initializeGame(gameId);
-const socket = io({query: {gameId}});
+    Simulations.initialize(gameId);
+  };
 
-const publish = event => {
-  console.log(event);
-  document.dispatchEvent(new CustomEvent(event.type, {detail: event}));
-};
+  const gameId = $gameId.dataset.gameId;
+  initializeGame(gameId);
+  const socket = io({query: {gameId}});
 
-socket.on('message', event => publish(event));
-socket.on('replay', events => events.forEach(event => publish(event)));
+  const publish = event => {
+    console.log(event);
+    document.dispatchEvent(new CustomEvent(event.type, {detail: event}));
+  };
+
+  socket.on('message', event => publish(event));
+  socket.on('replay', events => events.forEach(event => publish(event)));
+}
+
