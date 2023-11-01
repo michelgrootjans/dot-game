30262b17ad16b6987527560228fc99c788c31bb3
Michel Grootjans <michel.grootjans@gmail.com>
2023-11-01 11:08:15 +0100

    small refactor

--- web/src/Card.js
+++ web/src/Card.js
@@ -2,21 +2,21 @@ const API = require('./API')
 const $cardTemplate = document.getElementById('card-template')
 
 const create = ({ gameId, taskId, color, payload }) => {
-  const card = $cardTemplate.content.firstElementChild.cloneNode(true)
+  const $card = $cardTemplate.content.firstElementChild.cloneNode(true)
 
-  card.dataset.taskId = taskId
-  card.payload = payload || {}
+  $card.dataset.taskId = taskId
+  $card.payload = payload || {}
 
-  card.setAttribute('style', `background: ${color};`)
-  card.querySelector('.move-button')?.addEventListener('click', (event) => {
+  $card.setAttribute('style', `background: ${color};`)
+  $card.querySelector('.move-button')?.addEventListener('click', (event) => {
     event.preventDefault()
     API(gameId).task.move(taskId, { payload })
   })
-  card.querySelector('.reject-button')?.addEventListener('click', (event) => {
+  $card.querySelector('.reject-button')?.addEventListener('click', (event) => {
     event.preventDefault()
     API(gameId).task.reject(taskId, { payload })
   })
-  return card
+  return $card
 }
 
 const find = ({ taskId }) => document.querySelector(`[data-task-id="${taskId}"]`)
@@ -24,9 +24,9 @@ const findOrCreate = (detail) => find(detail) || create(detail)
 const remove = (detail) => find(detail)?.remove()
 
 const removeAll = () => {
-  const cards = document.getElementsByClassName('postit')
-  while (cards.length > 0) {
-    cards[0].parentNode.removeChild(cards[0])
+  const $cards = document.getElementsByClassName('postit')
+  while ($cards.length > 0) {
+    $cards[0].parentNode.removeChild($cards[0])
   }
 }
 
