515d13299c5ecbd62f25d5f3c9461cb525e0caf0
Michel Grootjans <michel.grootjans@gmail.com>
2025-01-08 13:56:41 +0100

    r!: isolate tasks from initial-5-player-state.js

--- application/domain/initial-state.js
+++ application/domain/initial-state.js
@@ -1,11 +1,15 @@
 const fourPlayers = require('./initial-4-player-state')
 const fivePlayers = require('./initial-5-player-state')
 
+const toId = (name) => name.split(' ').join('')
+const toTasks = names => names.map(name => ({name, id: toId(name)}));
+
 const initialState = (numberOfPlayers = 5) => {
   if (numberOfPlayers === 4 || numberOfPlayers === '4') {
     return fourPlayers()
   } else {
-    return fivePlayers()
+    const taskNames = ['To Do', 'Analysis', 'Development', 'Ops', 'QA']
+    return fivePlayers(toTasks(taskNames))
   }
 }
 
