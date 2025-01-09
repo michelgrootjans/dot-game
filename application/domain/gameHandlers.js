e7eace1d125a5285320b09cd700f2494dea7e604
Michel Grootjans <michel.grootjans@gmail.com>
2025-01-09 16:09:17 +0100

    Fail on insufficient tasks

--- application/domain/gameHandlers.js
+++ application/domain/gameHandlers.js
@@ -2,12 +2,16 @@ const { GameCreated } = require('../api/events/game')
 const initialState = require('./initial-state')
 const {DEFAULT_TASK_NAMES} = require("./defaults");
 
+const hasValue = (text) => {
+  if(!text) return false;
+  return text.trim().length > 0;
+};
+
 const CreateGameHandler = (games, publish) => ({
   execute: ({ gameId, state, taskNames = DEFAULT_TASK_NAMES }) => {
     if (games.find(gameId)) return
 
-    console.log({taskNames})
-    let newState = state || initialState({taskNames: taskNames})
+    const newState = state || initialState({taskNames: taskNames.filter(hasValue)});
 
     const game = { ...newState, gameId }
     games.add(game)
--- application/domain/initial-state.js
+++ application/domain/initial-state.js
@@ -5,13 +5,13 @@ const {DEFAULT_TASK_NAMES} = require("./defaults");
 const toId = (name) => name.split(' ').join('')
 const toTasks = names => names.map(name => ({title: name, id: toId(name)}));
 
-const initialState = ({taskNames} = {taskNames: DEFAULT_TASK_NAMES}) => {
-  console.log({taskNames})
+const initialState = ({taskNames = []} = {taskNames: DEFAULT_TASK_NAMES}) => {
   if (taskNames.length === 4) {
     return fourPlayers(toTasks(taskNames))
-  } else {
+  } else if(taskNames.length === 5) {
     return fivePlayers(toTasks(taskNames))
   }
+  throw 'Invalid parameters: task names'
 }
 
 module.exports = initialState
