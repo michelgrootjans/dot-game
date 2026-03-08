0087b16590c8df39219fe6c8fd74c14680bb90d0
Michel Grootjans <michel.grootjans@gmail.com>
2026-03-08 16:43:03 +0100

    Remove unused CHART_COLORS object from ScatterPlot.js to clean up the code.

--- web/src/ScatterPlot.js
+++ web/src/ScatterPlot.js
@@ -1,15 +1,5 @@
 const { Chart } = require('chart.js')
 
-// const CHART_COLORS = {
-//   red: 'rgb(255, 99, 132)',
-//   orange: 'rgb(255, 159, 64)',
-//   yellow: 'rgb(255, 205, 86)',
-//   green: 'rgb(75, 192, 192)',
-//   blue: 'rgb(54, 162, 235)',
-//   purple: 'rgb(153, 102, 255)',
-//   grey: 'rgb(201, 203, 207)'
-// };
-
 class WorkItem {
   constructor(id, dateStarted) {
     this.id = id
